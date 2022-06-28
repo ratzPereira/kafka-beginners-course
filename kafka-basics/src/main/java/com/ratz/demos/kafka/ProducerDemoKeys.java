5b7088c00b77b536da67a87efcd76e4329eff42e
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-06-28 12:15:38 +0000

    created Producer with keys

--- kafka-basics/src/main/java/com/ratz/demos/kafka/ProducerDemoKeys.java
+++ kafka-basics/src/main/java/com/ratz/demos/kafka/ProducerDemoKeys.java
@@ -1,15 +1,17 @@
 package com.ratz.demos.kafka;
 
-import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
-public class ProducerDemoWithCallBack {
+public class ProducerDemoKeys {
 
-  private static final Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallBack.class.getSimpleName());
+  private static final Logger logger = LoggerFactory.getLogger(ProducerDemoKeys.class.getSimpleName());
 
   public static void main(String[] args) {
 
@@ -24,31 +26,28 @@ public class ProducerDemoWithCallBack {
     KafkaProducer<String,String> producer = new KafkaProducer<>(properties);
 
 
-
     for (int i = 0; i < 10; i++) {
 
+      String topic = "demo_java";
+      String value = "Hello its me, the number " + i;
+      String key = "id_" + i;
+
       //producer record
-      ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java", "hello" + i );
+      ProducerRecord<String,String> producerRecord = new ProducerRecord<>(topic, key, value);
+
 
 
       //send data - async
       producer.send(producerRecord, (metadata, exception) -> {
 
         //executes everytime a record is successfully sent or exception is thrown
-        if (exception == null) {
-          logger.info("Got metadata " + "Topic, Partition, Offset, Timestamp  " + "\n"
-              + metadata.topic() + "\n" + metadata.partition() + "\n" + metadata.offset() + "\n" + metadata.timestamp());
+        if(exception == null){
+          logger.info("Got metadata " + "Topic, Partition, Offset, Timestamp, Key  " + "\n"
+              + metadata.topic() + "\n" +  metadata.partition() + "\n" +  metadata.offset()+ "\n" + metadata.timestamp() + "\n" + producerRecord.key());
         } else {
           logger.error("Error producing");
         }
-
-        try {
-          Thread.sleep(3000);
-        } catch (InterruptedException e) {
-          e.printStackTrace();
-        }
       });
-
     }
 
     //flush producer
