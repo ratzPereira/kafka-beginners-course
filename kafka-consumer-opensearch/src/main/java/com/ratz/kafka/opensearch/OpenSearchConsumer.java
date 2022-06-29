44f819daf10146e1291b496d7d596d9f2d8c851d
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-06-29 15:32:33 +0000

    added offsets commit manually

--- kafka-consumer-opensearch/src/main/java/com/ratz/kafka/opensearch/OpenSearchConsumer.java
+++ kafka-consumer-opensearch/src/main/java/com/ratz/kafka/opensearch/OpenSearchConsumer.java
@@ -80,7 +80,7 @@ public class OpenSearchConsumer {
     properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
     properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
     properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
-    properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
+    properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false"); // not going commit automatically
 
     // create consumer
     return new KafkaConsumer<>(properties);
@@ -163,6 +163,10 @@ public class OpenSearchConsumer {
             log.error(e.getMessage());
           }
         }
+
+        //commit offsets after batch is consumed
+        consumer.commitSync();
+        log.info("Offsets have been committed!");
       }
     }
 
